98a0aa7e8c678f41ca64eec7d57a08a48178edf3
Ivan Iliev <mr.i.iliev@gmail.com>
2024-01-17 20:50:13 +0200

    remove unnecessary code for version 2

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,9 @@
 #include <BH1750.h>
 #include <SD.h>
 #include <SPI.h>
-#include <ESPmDNS.h>
-#include <Esp.h>
-#include <Firebase_ESP_Client.h>
-#include "time.h"
-#include "addons/TokenHelper.h"
-#include "addons/RTDBHelper.h"
+#include <LittleFS.h>
+
+
 
 
 
@@ -32,44 +29,14 @@
 
 #define DHT_TYPE DHT11
 
-int last = HIGH;
+
 int state;
 
 
 float luxRead;
 float advice;
 float soil;
-IPAddress ip;
 
-#define USER_EMAIL "mr.i.iliev@gmail.com"
-#define USER_PASSWORD "qwertyui"
-#define API_KEY "AIzaSyC-A0MfAzIYWH0_iOIq7Nd5MQDIXOMciXk"
-#define DATABASE_URL "https://flowersathome-2817b-default-rtdb.firebaseio.com/"
-
-FirebaseData fbdo;
-FirebaseAuth auth;
-FirebaseConfig config;
-
-String uid;
-String databasePath;
-
-String tempPath = "/temperature";
-String humPath = "/humidity";
-String lightPath = "/light";
-String soilPath = "/soil";
-String saltPath = "/salt";
-String batPath = "/battery";
-String timePath = "/timestamp";
-String parentPath;
-
-int timestamp;
-
-FirebaseJson json;
-const char* ntpServer = "pool.ntp.org";
-
-unsigned long sendDataPrevMillis = 0;
-unsigned long timerDelay = 180000;
-bool signupOK = false;
 
 BH1750 lightMeter(0x23);
 DHT dht(DHT_PIN, DHT_TYPE);
@@ -174,17 +141,6 @@ String readSalt()
   return String(humi);
 }
 
-unsigned long getTime() {
-  time_t now;
-  struct tm timeinfo;
-  if (!getLocalTime(&timeinfo)) {
-    //Serial.println("Failed to obtain time");
-    return(0);
-  }
-  time(&now);
-  return now;
-}
-
 
 void setup() {
 
@@ -197,6 +153,7 @@ void setup() {
   digitalWrite(POWER_CTRL, 1);
   delay(1000);
 
+  
 
   
 
@@ -226,108 +183,31 @@ void setup() {
   }
 
 
-  WiFiManager wm;
 
   bool res;
 
+
+  WiFiManager wm;
   res = wm.autoConnect("Flowers At Home", "password"); // password protected ap
   wm.startWebPortal();
  // wm.resetSettings();
 
+state = digitalRead(USER_BUTTON);
+
+  if(state==LOW){
+     wm.resetSettings();
+  }
 
   if (!res) {
-
     Serial.println("Failed to connect");
-
-    // ESP.restart();
-
+    ESP.restart();
   }
 
-  configTime(0, 0, ntpServer);
-
-  config.api_key = API_KEY;
-  auth.user.email = USER_EMAIL;
-  auth.user.password = USER_PASSWORD;
-  config.database_url = DATABASE_URL;
-
-  /* Assign the callback function for the long running token generation task */
-  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
-  
-  Firebase.begin(&config, &auth);
-  Serial.println("Getting User UID");
-  while ((auth.token.uid) == "") {
-    Serial.print('.');
-    delay(1000);
-  }
-  // Print user UID
-  uid = auth.token.uid.c_str();
-  Serial.print("User UID: ");
-  Serial.println(uid);
-  databasePath = "/readings";
-
-
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
-    request->send(SPIFFS, "/index.html");
-  });
-   server.on("/favicon", HTTP_GET, [](AsyncWebServerRequest * request) {
-    request->send(SPIFFS, "/favicon.ico", "image/ico");
-  });
-   server.on("/logo", HTTP_GET, [](AsyncWebServerRequest * request) {
-    request->send(SPIFFS, "/logo.png", "image/png");
-  });
-  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest * request) {
-    request->send_P(200, "text/plain", readTemp().c_str());
-  });
-  server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest * request) {
-    request->send_P(200, "text/plain", readHum().c_str());
-  });
-  server.on("/light", HTTP_GET, [](AsyncWebServerRequest * request) {
-    request->send_P(200, "text/plain", readLight().c_str());
-  });
-
-  server.on("/soil", HTTP_GET, [](AsyncWebServerRequest * request) {
-    request->send_P(200, "text/plain", readSoil().c_str());
-  });
-
-  server.on("/salt", HTTP_GET, [](AsyncWebServerRequest * request) {
-    request->send_P(200, "text/plain", readSalt().c_str());
-  });
-
-  server.on("/battery", HTTP_GET, [](AsyncWebServerRequest * request) {
-    request->send_P(200, "text/plain", String(readBattery()).c_str());
-  });
-
-  server.begin();
 }
 
 void loop() {
-  if (Firebase.ready() && (millis() - sendDataPrevMillis > timerDelay || sendDataPrevMillis == 0)){
-    sendDataPrevMillis = millis();
 
-    timestamp = getTime();
-    Serial.print ("time: ");
-    Serial.println (timestamp);
-
-    String MAC = WiFi.macAddress();
-    parentPath= databasePath + "/" + MAC;
-    json.set(tempPath.c_str(),readTemp().c_str());
-    json.set(humPath.c_str(),readHum().c_str());
-    json.set(lightPath.c_str(),readLight().c_str());
-    json.set(soilPath.c_str(),readSoil().c_str());
-    json.set(saltPath.c_str(),readSalt().c_str());
-    json.set(batPath.c_str(),String(readBattery()).c_str());
-    json.set(timePath.c_str(),String(timestamp));
-     Serial.printf("Set json... %s\n", Firebase.RTDB.setJSON(&fbdo, parentPath.c_str(), &json) ? "ok" : fbdo.errorReason().c_str());
-  }
-
-
-  state = digitalRead(USER_BUTTON);
-
-  if(state==LOW){
-     Serial.println("LOW");
-  }else{
-    Serial.println("high");
-}
+ 
 
 }
 
