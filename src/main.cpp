8c4b0f0034a9a7c017a9350d3ebf262dfdcd13f2
Ivan Iliev <mr.i.iliev@gmail.com>
2024-01-26 18:13:37 +0200

    fixing timestamp

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,28 @@
 #include <WiFiManager.h>
 #include <WiFi.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
 #include <ESPAsyncWebServer.h>
 #include <AsyncTCP.h>
+#include <ArduinoJson.h>
 #include <DNSServer.h>
 #include <SPIFFS.h>
 #include <Arduino.h>
+#include <ESP32Time.h>
+#include <stdint.h>
 #include <DHT.h>
 #include <Wire.h>
 #include <BH1750.h>
 #include <SD.h>
 #include <SPI.h>
 #include <LittleFS.h>
+#include <WiFiClientSecure.h>
+#include <HTTPClient.h>
 #include <stdio.h>
 #include <string.h>
 #include "esp_system.h"
 #include "esp_wifi.h"
+#include "time.h"
 
 
 
@@ -35,6 +43,11 @@
 //#define THRESHOLD   1
 #define BUTTON_PIN_BITMASK 0x800000000 // 2^35 in hex
 
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
+String timeStamp;
+uint64_t milisec;
+
 DNSServer dnsServer;
 
 AsyncWebServer server(80);
@@ -44,12 +57,53 @@ const char* PARAM_INPUT_2 = "pass";
 const char* PARAM_INPUT_3 = "username";
 const char* PARAM_INPUT_4 = "userpass";
 
+const char*  HTTPSServer = "https://www.botanica-wellness.com/report.php?mac=11:22:33:44:55:66&user=test@test.bg&pass=test";
+
+const char* test_root_ca= \
+  "-----BEGIN CERTIFICATE-----\n" \
+  "MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw\n" \
+  "TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh\n" \
+  "cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4\n" \
+  "WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu\n" \
+  "ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY\n" \
+  "MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc\n" \
+  "h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+\n" \
+  "0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U\n" \
+  "A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW\n" \
+  "T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH\n" \
+  "B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC\n" \
+  "B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv\n" \
+  "KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn\n" \
+  "OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn\n" \
+  "jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw\n" \
+  "qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI\n" \
+  "rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV\n" \
+  "HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq\n" \
+  "hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL\n" \
+  "ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ\n" \
+  "3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK\n" \
+  "NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5\n" \
+  "ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur\n" \
+  "TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC\n" \
+  "jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc\n" \
+  "oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq\n" \
+  "4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA\n" \
+  "mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d\n" \
+  "emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=\n" \
+  "-----END CERTIFICATE-----\n"; 
+
+
+
 
 String ssid;
 String pass;
 String username;
 String userpass;
+const char* ntpServer = "ntp.rit.edu";
 
+
+
+JsonDocument postJson;
 
 const char* ssidPath = "/ssid.conf";
 const char* passPath = "/pass.conf";
@@ -94,24 +148,18 @@ void print_wakeup_reason(){
     default : Serial.printf("Wakeup was not caused by deep sleep: %d\n",wakeup_reason); break;
   }
 }
-/*void print_wakeup_touchpad(){
-  touchPin = esp_sleep_get_touchpad_wakeup_status();
-    switch(touchPin)
-    {
-      case 0  : Serial.println("Touch detected on GPIO 4"); break;
-      case 1  : Serial.println("Touch detected on GPIO 0"); break;
-      case 2  : Serial.println("Touch detected on GPIO 2"); break;
-      case 3  : Serial.println("Touch detected on GPIO 15"); break;
-      case 4  : Serial.println("Touch detected on GPIO 13"); break;
-      case 5  : Serial.println("Touch detected on GPIO 12"); break;
-      case 6  : Serial.println("Touch detected on GPIO 14"); break;
-      case 7  : Serial.println("Touch detected on GPIO 27"); break;
-      case 8  : Serial.println("Touch detected on GPIO 33"); break;
-      case 9  : Serial.println("Touch detected on GPIO 32"); break;
-      default : Serial.println("Wakeup not by touchpad"); break;
-    }
-  
-}*/
+
+unsigned long Get_Epoch_Time() {
+  time_t now;
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo)) {
+    Serial.println("Failed to obtain time");
+    return(0);
+  }
+  time(&now);
+  return now;
+}
+
 String readTemp() {
 
   float t = dht.readTemperature();
@@ -301,6 +349,7 @@ void setup() {
   pinMode(USER_BUTTON,INPUT);
   dht.begin();
   lightMeter.begin();
+  
 
   initSPIFFS();
   ssid = readFile(SPIFFS, ssidPath);
@@ -352,11 +401,12 @@ void setup() {
 
 
 if(initWiFi()) {
-    // izprashtame https zaqvka tuk
- float bat = readBattery();
+  
+  timeClient.begin();
+  //configTime(0, 0, ntpServer);
+  float bat = readBattery();
   Serial.println("Battery level");
   Serial.println(bat);
-
   Serial.print("**********************************");
   Serial.print("Printing Data of ALl the Sensors");
   Serial.println("**********************************");
@@ -381,6 +431,66 @@ if(initWiFi()) {
 
   float batt = readBattery();
   Serial.print("Battery - "); Serial.println(batt);
+
+
+  //unsigned long epochTime; 
+  //uint64_t milisecTime;
+
+  //epochTime = Get_Epoch_Time();  
+
+
+  timeClient.update();
+  timeStamp = timeClient.getEpochTime();
+  
+  Serial.print("Time - "); Serial.println(timeStamp);
+  
+  milisec = timeStamp.toInt();
+  milisec *= 1000;
+
+  Serial.print("TimeMil - "); Serial.println(milisec);
+  String resJson;
+  postJson["battery"] = bat;
+  postJson["humidity"] = h;
+  postJson["light"] = luxRead;
+  postJson["salt"] = salt;
+  postJson["soil"] = soil;
+  postJson["temperature"] = t;
+  postJson["timestamp"] = milisec;
+  serializeJson(postJson, resJson);
+
+  WiFiClientSecure *client = new WiFiClientSecure;
+  if(client) {
+    // set secure client with certificate
+    client->setCACert(test_root_ca);
+    //create an HTTPClient instance
+    HTTPClient https;
+
+    //Initializing an HTTPS communication using the secure client
+    Serial.print("[HTTPS] begin...\n");
+    if (https.begin(*client, HTTPSServer)) {  // HTTPS
+      Serial.print("[HTTPS] POST...\n");
+      
+      https.addHeader("Content-Type", "application/json");
+      int httpCode = https.POST(resJson);
+      // httpCode will be negative on error
+      if (httpCode > 0) {
+      // HTTP header has been send and Server response header has been handled
+       Serial.printf("[HTTPS] POST... code: %d\n", httpCode);
+
+        if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+          // print server response payload
+          String payload = https.getString();
+          Serial.println(payload);
+        }
+      }
+      else {
+        Serial.printf("[HTTPS] POST... failed, error: %s\n", https.errorToString(httpCode).c_str());
+      }
+      https.end();
+    }
+  } else {
+    Serial.printf("[HTTPS] Unable to connect\n");
+  }
 
 
 
@@ -479,7 +589,6 @@ if(initWiFi()) {
 
 
 
-
   state = digitalRead(USER_BUTTON);
   if(state==HIGH){
     //SPIFFS.remove("/ssid.conf");
@@ -499,5 +608,6 @@ void loop() {
    if (state == LOW) {
     Serial.println("natisnah natisnah natisnah");
   }
+  
 }
 
